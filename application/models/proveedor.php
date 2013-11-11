7e0e7ad4da824b2a810ffb8737dde591d54e3d14
cherra <jorge@matica.mx>
2013-11-11 10:59:25 -0600

    Modelo para proveedores

---
+++ application/models/proveedor.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Description of proveedor
+ *
+ * @author cherra
+ */
+class Proveedor extends CI_Model {
+    
+    private $tbl = 'SUPPLIERS';
+    
+    /*
+     * Cuenta todos los registros utilizando un filtro de busqueda
+     */
+    function count_all( $filtro = NULL ) {
+        if(!empty($filtro)){
+            $filtro = explode(' ', $filtro);
+            foreach($filtro as $f){
+                $this->db->or_like('ID',$f);
+                $this->db->or_like('NAME',$f);
+            }
+        }
+        $query = $this->db->get($this->tbl);
+        return $query->num_rows();
+    }
+    
+    /**
+     *  Obtiene todos los registros de la tabla
+     */
+    function get_all() {
+        $this->db->order_by('ID','asc');
+        return $this->db->get($this->tbl);
+    }
+    
+    /**
+    * Cantidad de registros por pagina
+    */
+    function get_paged_list($limit = NULL, $offset = 0, $filtro = NULL) {
+        if(!empty($filtro)){
+            $filtro = explode(' ', $filtro);
+            foreach($filtro as $f){
+                $this->db->or_like('ID',$f);
+                $this->db->or_like('NAME',$f);
+            }
+        }
+        $this->db->order_by('NAME','asc');
+        return $this->db->get($this->tbl, $limit, $offset);
+    }
+    
+    /**
+    * Obtener por id
+    */
+    function get_by_id($id) {
+        $this->db->where('ID', $id);
+        return $this->db->get($this->tbl);
+    }
+    
+    /**
+    * Alta
+    */
+    function save( $datos ) {
+        $this->db->insert($this->tbl, $datos);
+        return $this->db->insert_id();
+    }
+
+    /**
+    * Actualizar por id
+    */
+    function update($id, $datos) {
+        $this->db->where('ID', $id);
+        $this->db->update($this->tbl, $datos);
+    }
+
+    /**
+    * Eliminar por id
+    */
+    function delete($id) {
+        $this->db->where('ID', $id);
+        $this->db->delete($this->tbl);
+    } 
+}
+?>
