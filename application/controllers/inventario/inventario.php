902a316df5f5939c9e1262b1fd15d490ac8dbab8
cherra <jorge@matica.mx>
2013-11-12 19:02:12 -0600

    Formulario para capturar entradas a almacén con impresión de etiquetas.

--- application/controllers/inventario/inventario.php
+++ application/controllers/inventario/inventario.php
@@ -16,5 +16,137 @@ class Inventario extends CI_Controller {
     public function index(){
         $this->load->view('vacio');
     }
+    
+    public function entradas( $offset = 0 ){
+        $this->load->model('stock','s');
+        $this->load->model('producto','p');
+        $this->load->model('sucursal','su');
+        $this->load->model('usuario','u');
+        
+        $this->config->load("pagination");
+    	
+        $data['titulo'] = 'Entradas <small>Lista</small>';
+    	$data['link_add'] = $this->folder.$this->clase.'entradas_agregar/'.$offset;
+    	$data['action'] = $this->folder.$this->clase.'entradas';
+        
+        // Filtro de busqueda (se almacenan en la sesión a través de un hook)
+        $filtro = $this->session->userdata('filtro');
+        if($filtro)
+            $data['filtro'] = $filtro;
+        
+        $page_limit = $this->config->item("per_page");
+    	$datos = $this->s->get_paged_list_entradas($page_limit, $offset, $filtro)->result();
+    	
+        // generar paginacion
+    	$this->load->library('pagination');
+    	$config['base_url'] = site_url($this->folder.$this->clase.'entradas');
+    	$config['total_rows'] = $this->s->count_all_entradas($filtro);
+    	$config['per_page'] = $page_limit;
+    	$config['uri_segment'] = 4;
+    	$this->pagination->initialize($config);
+    	$data['pagination'] = $this->pagination->create_links();
+    	
+    	// generar tabla
+    	$this->load->library('table');
+    	$this->table->set_empty('&nbsp;');
+    	$tmpl = array ( 'table_open' => '<table class="' . $this->config->item('tabla_css') . '" >' );
+    	$this->table->set_template($tmpl);
+    	$this->table->set_heading('Fecha', 'Producto', 'C', array('data'=> 'Sucursal', 'class' => 'hidden-xs'), '', '');
+    	foreach ($datos as $d) {
+            $producto = $this->p->get_by_id($d->PRODUCT)->row();
+            $sucursal = $this->su->get_by_id($d->LOCATION)->row();
+            //$usuario = $this->su->get_by_id($d->PERSON)->row();
+            $this->table->add_row(
+                    $d->DATENEW,
+                    $producto->NAME,
+                    $d->UNITS,
+                    array('data'=> $sucursal->NAME, 'class' => 'hidden-xs'),
+                    //array('data'=> (!empty($usuario->NAME) ? $usuario->NAME : ''), 'class' => 'hidden-xs'),
+                    anchor($this->folder.$this->clase.'entradas_ver/' . $d->ID . '/' . $offset, '<span class="glyphicon glyphicon-edit"></span>','title="Editar"'),
+                    anchor($this->folder.$this->clase.'entradas_borrar/' . $d->ID . '/' . $offset, '<span class="glyphicon glyphicon-remove"></span>','title="Borrar"')
+            );
+    	}
+    	$data['table'] = $this->table->generate();
+    	
+    	$this->load->view('lista', $data);
+    }
+    
+    /*
+     * Agregar una entrada
+     */
+    
+    public function entradas_agregar( $offset = 0 ){
+        $this->load->model('stock','s');
+        $this->load->model('sucursal','su');
+        $this->load->model('proveedor','p');
+        $this->load->model('producto','pr');
+        $this->load->model('categoria','c');
+        
+        if( $this->input->is_ajax_request() ){
+            if ( ($datos = $this->input->post()) ) {
+                $this->load->library('uuid');
+                $this->load->library('etiqueta');  // Para generar las etiquetas
+                $this->load->helper('file');
+
+                $data['proveedor'] = $this->p->get_by_id($datos['SUPPLIER'])->row();
+                $data['sucursal'] = $this->su->get_by_id($datos['LOCATION'])->row();
+                $data['categoria'] = $this->c->get_by_id($datos['CATEGORY'])->row();
+
+                /*
+                 * Se genera el nuevo producto
+                 */
+                $this->db->trans_start(); // Inicio de transacción en la base de datos
+                $id_producto = $this->uuid->v4();
+                $codigo = $this->pr->genera_codigo($datos['SUPPLIER'], $datos['CATEGORY']);
+                $producto = array(
+                    'ID' => $id_producto,
+                    'CODE' => $codigo,
+                    'NAME' => $datos['NAME'],
+                    'PRICESELL' => $datos['PRICE'],
+                    'CATEGORY' => $datos['CATEGORY'],
+                    'SUPPLIER' => $datos['SUPPLIER'],
+                    'TAXCAT' => '000'
+                );
+                if( $this->pr->save($producto) > 0 ){
+                    $id_entrada = $this->uuid->v4();
+                    $entrada = array(
+                        'ID' => $id_entrada,
+                        'DATENEW' => date('Y-m-d H:i:s'),
+                        'REASON' => '1',
+                        'LOCATION' => $datos['LOCATION'],
+                        'PRODUCT' => $id_producto,
+                        'UNITS' => $datos['UNITS'],
+                        'PRICE' => $datos['PRICE']
+                    );
+
+                    if( $this->s->save($entrada) > 0 ){
+                        $etiqueta = $this->etiqueta->genera($this->config->item('label_format'), $datos['NAME'], $datos['PRICE'], $codigo, '', $datos['UNITS'], $datos['REFERENCE']);
+                        if($etiqueta){
+                            if(write_file($this->config->item('asset_path').$this->config->item('label_file'), $etiqueta)){
+                                echo "OK";
+                            }else
+                                echo "Error: No se pudo escribir en disco el archivo de la etiqueta.";
+                            //exec('lp -d '.$this->config->item('label_printer').' -h '.$this->config->item('label_host').' -o raw labels/'.$this->config->item('label_file'));
+                        }else{
+                            echo "Error al generar la etiqueta: ".$this->config->item('asset_path').$this->config->item('label_format');
+                        }
+                    }else{
+                        echo "Error al guardar la etiqueta en disco";
+                    }
+                }
+                $this->db->trans_complete();
+            }
+        }else{
+            $data['titulo'] = 'Entradas <small>Registro nuevo</small>';
+            $data['link_back'] = $this->folder.$this->clase.'entradas/'.$offset;
+
+            $data['action'] = $this->folder.$this->clase.'entradas_agregar/'.$offset;
+
+            $data['categorias'] = $this->c->get_all()->result();
+            $data['proveedores'] = $this->p->get_all()->result();
+            $data['sucursales'] = $this->su->get_all()->result();
+            $this->load->view('inventario/entradas/formulario', $data);
+        }
+    }
 }
 ?>
---
+++ application/views/inventario/entradas/formulario.php
@@ -0,0 +1,216 @@
+<?php echo form_open($action, array('class' => 'form-horizontal', 'name' => 'form', 'id' => 'form', 'role' => 'form')) ?>
+    <div class="form-group">
+        <div class="col-xs-6">
+            <?php echo anchor($link_back,'<span class="'.$this->config->item('icono_regresar').'"></span> Regresar'); ?>
+        </div>
+        <div class="col-xs-6">
+            <button type="submit" id="guardar" class="btn btn-primary pull-right"><span class="<?php echo $this->config->item('icono_guardar'); ?>"></span> Guardar</button>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2" for="LOCATION">Sucursal</label>
+        <div class="col-sm-8 col-md-6 col-lg-4">
+            <select name="LOCATION" class="form-control required">
+                <option value="">Selecciona una sucursal...</option>
+                <?php
+                foreach($sucursales AS $s){
+                ?>
+                    <option value="<?php echo $s->ID; ?>" <?php if(isset($sucursal->ID) && ($sucursal->ID == $s->ID)) echo "selected"; ?>><?php echo $s->NAME; ?></option>
+                <?php
+                }
+                ?>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2" for="SUPPLIER">Proveedor</label>
+        <div class="col-sm-8 col-md-6 col-lg-4">
+            <select name="SUPPLIER" class="form-control required">
+                <option value="">Selecciona un proveedor...</option>
+                <?php
+                foreach($proveedores AS $p){
+                ?>
+                    <option value="<?php echo $p->ID; ?>" <?php if(isset($proveedor->ID) && ($proveedor->ID == $p->ID)) echo "selected"; ?>><?php echo $p->PREFIX; ?></option>
+                <?php
+                }
+                ?>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2" for="CATEGORY">Categoría</label>
+        <div class="col-sm-8 col-md-6 col-lg-4">
+            <select name="CATEGORY" class="form-control required">
+                <option value="">Selecciona una categoría...</option>
+                <?php
+                foreach($categorias AS $c){
+                ?>
+                    <option value="<?php echo $c->ID; ?>" <?php if(isset($categoria->ID) && ($categoria->ID == $c->ID)) echo "selected"; ?>><?php echo $c->NAME; ?></option>
+                <?php
+                }
+                ?>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2" for="NAME">Nombre artículo</label>
+        <div class="col-sm-8 col-md-6 col-lg-4">
+            <input type="text" name="NAME" id="NAME" class="form-control required" value="<?php echo (isset($producto->NAME) ? $producto->NAME : ''); ?>" placeholder="Artículo">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2" for="REFERENCE">Código proveedor</label>
+        <div class="col-sm-8 col-md-6 col-lg-4">
+            <input type="text" name="REFERENCE" class="form-control required" value="<?php echo (isset($producto->REFERENCE) ? $producto->REFERENCE : ''); ?>" placeholder="Código proveedor">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2" for="UNITS">Cantidad</label>
+        <div class="col-sm-8 col-md-6 col-lg-4">
+            <input type="text" name="UNITS" class="form-control required" value="<?php echo (isset($datos->UNITS) ? $datos->UNITS : ''); ?>" placeholder="Cantidad">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2" for="PRICE">Precio</label>
+        <div class="col-sm-8 col-md-6 col-lg-4">
+            <input type="text" name="PRICE" class="form-control required" value="<?php echo (isset($datos->PRICE) ? $datos->PRICE : ''); ?>" placeholder="Precio">
+        </div>
+    </div>
+<?php echo form_close(); ?>
+<div class="alert alert-success alert-dismissable" style="display: none;" id="alerta">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><span id="mensaje"></span>
+</div>
+<applet id="qz" archive="<?php echo asset_url().'qz-print/qz-print.jar'; ?>" name="QZ Print Plugin" code="qz.PrintApplet.class" width="1" height="1">
+<!--    <param name="jnlp_href" value="<?php echo asset_url().'qz-print/qz-print_jnlp.jnlp'; ?>">-->
+<!--    <param name="cache_option" value="plugin">
+    <param name="disable_logging" value="false">-->
+</applet>
+
+<script type="text/javascript">
+//
+//function getPath() {
+//    var path = window.location.href;
+//    return path.substring(0, path.lastIndexOf("/")) + "/";
+//}
+
+var qz;
+//var imprimir = 0;
+    
+function monitorFinding() {
+
+    if (qz) {
+       if (!qz.isDoneFinding()) {
+          window.setTimeout('monitorFinding()', 100);
+       } else {
+          var printer = qz.getPrinter();
+          console.log(printer == null ? "Printer not found" : "Printer \"" + printer + "\" found and selected.");
+       }
+    } else {
+        alert("Error al cargar el applet para impresión.\nNo se pueden imprimir etiquetas.");
+    }
+}
+
+function findPrinter() {
+    if (qz) {
+       // Searches for locally installed printer wiht specified name
+       qz.findPrinter("<?php echo $this->config->item('label_printer'); ?>");
+    }
+
+    // *Note:  monitorFinding() still works but is too complicated and
+    // outdated.  Instead create a JavaScript  function called 
+    // "qzDoneFinding()" and handle your next steps there.
+    monitorFinding();
+ }
+
+function monitorPrinting() {
+
+    if (qz) {
+        if (!qz.isDonePrinting()) {
+           window.setTimeout('monitorPrinting()', 100);
+        } else {
+           var e = qz.getException();
+           console.log(e ? "Exception occured: " + e.getLocalizedMessage() : "Impresión correcta!");
+           qz.clearException();
+        }
+    } else {
+        alert("Error al cargar el applet para impresión.\nNo se pueden imprimir etiquetas.");
+    }
+}
+
+function printFile(file) {
+
+    if (qz) {
+       // Using qz-print's "appendFile()" function, a file containg your raw EPL/ZPL
+       // can be sent directly to the printer
+       // Example: 
+       //     qz.appendFile("http://yoursite/zpllabel.txt"); // ...etc
+       qz.appendFile(file);
+       qz.print();
+    }else{
+        alert("Error al cargar el applet para impresión de etiquetas");
+    }
+
+    // *Note:  monitorPrinting() still works but is too complicated and
+    // outdated.  Instead create a JavaScript  function called 
+    // "qzDonePrinting()" and handle your next steps there.
+    monitorPrinting();
+}
+
+qz = document.getElementById('qz');
+if(qz){
+    findPrinter();
+}
+
+var validator = $('#form').validate({
+    errorClass: "has-error",
+    validClass: "has-success",
+    rules: {
+        confirmar_password: {
+            equalTo: "#APPPASSWORD"
+        }
+    },
+    highlight: function(element, errorClass, validClass) {
+        $(element).parent().parent().addClass(errorClass).fadeOut(function() {
+          $(element).parent().parent().fadeIn();
+        });
+    },
+    unhighlight: function(element, errorClass, validClass){
+        $(element).parent().parent().removeClass(errorClass);
+    },
+    submitHandler: function(form){
+        guardar_producto();
+    }
+});
+
+function guardar_producto(){
+    console.log($('#form').serialize());
+
+    $.ajax({
+        url: "<?php echo site_url($action); ?>",
+        type: 'post',
+        data: $('#form').serialize(),
+        dataType: 'text'
+    }).done(function(respuesta){
+        console.log(respuesta);
+        if(respuesta == 'OK'){
+            printFile("<?php echo asset_url().$this->config->item('label_file'); ?>");
+            $('#mensaje').text('Registro creado con éxito');
+            if($('#alerta').hasClass('alert-danger'))
+                $('#alerta').removeClass('alert-danger').addClass('alert-success');
+        }else{
+            if($('#alerta').hasClass('alert-success'))
+                $('#alerta').removeClass('alert-success').addClass('alert-danger');
+            $('#mensaje').text('Error al registrar el artículo!');
+        }
+        
+        $('.alert').css('display','block');
+    }).fail(function(){
+        console.log("Error al intentar guardar la compra");
+    });
+
+    validator.resetForm();
+}
+
+$('#NAME').focus();
+
+</script>
